f879227a55d91293f8c8bf55bfe5f727625f2376
Anton Kulikov <anton_kulikov@epam.com>
2019-09-11 11:37:00 +0300

    update list of available games

--- CodingDojo/server/src/main/webapp/resources/js/admin.js
+++ CodingDojo/server/src/main/webapp/resources/js/admin.js
@@ -32,7 +32,7 @@ function defaultRegistrationSettings() {
         showData4: true,
         defaultGame: null,
         gameTypes: {
-            icancode: ['Contest']        
+            icancode: ['Contest', 'JavaScript']        
         }
     };
 }
